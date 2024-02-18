74d65e27726e72d5c10f7a3606cf802fa30f5aaf
N-011 <noah171202@gmail.com>
2024-02-19 00:53:32 +0400

    feat: added text to centered box

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let lenis;
 
 const text = document.querySelector('.hero-text');
 const headerImg = document.querySelector('.header-img');
-const logo = document.querySelector('#nav-logo');
+// const logo = document.querySelector('#nav-logo');
 const floatingText = document.querySelector('.floating-box');
 
 const init = () => {
@@ -15,8 +15,8 @@ const init = () => {
   lenis.on('scroll', ({ scroll }) => {
     text.style.transform = `translate3D(${(-scroll*5)}px, 0, 0)`;
     headerImg.style.transform = `translate3D(0, ${(-scroll*4)}px, 0)`;
-    logo.style.transform = `translate3D(${(scroll/5)}px, 0, 0)`;
-    floatingText.style.cssText = `transform: translate3D(0, ${(-scroll/3)}px, 0); filter: blur(${(scroll/50)}px);`;
+    // logo.style.transform = `translate3D(${(scroll/5)}px, 0, 0)`;
+    floatingText.style.cssText = `transform: translate3D(0, ${(-scroll/3)}px, 0); filter: blur(${(scroll/80)}px);`;
   });
 
   function raf(time) {
