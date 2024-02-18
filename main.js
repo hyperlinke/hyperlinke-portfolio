c99f01da03257a2eebc55622aff7cdf4f394ace6
N-011 <noah171202@gmail.com>
2024-02-18 20:39:36 +0400

    feat: added scroll blur effect

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let lenis;
 const text = document.querySelector('.hero-text');
 const headerImg = document.querySelector('.header-img');
 const logo = document.querySelector('#nav-logo');
+const floatingText = document.querySelector('.floating-box');
 
 const init = () => {
 
@@ -15,6 +16,7 @@ const init = () => {
     text.style.transform = `translate3D(${(-scroll*5)}px, 0, 0)`;
     headerImg.style.transform = `translate3D(0, ${(-scroll*4)}px, 0)`;
     logo.style.transform = `translate3D(${(scroll/5)}px, 0, 0)`;
+    floatingText.style.cssText = `transform: translate3D(0, ${(-scroll/3)}px, 0); filter: blur(${(scroll/50)}px);`;
   });
 
   function raf(time) {
