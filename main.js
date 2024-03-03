ffafe031e08a5b04e8fefa10ccb6bb6e8fbc04c9
N-011 <noah171202@gmail.com>
2024-03-03 20:58:53 +0400

    feat: added content

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const text = document.querySelector('.hero-text');
 const headerImg = document.querySelector('.header-img');
 // const logo = document.querySelector('#nav-logo');
 const floatingText = document.querySelector('.floating-box');
+const headerTwoText = document.querySelector('.header-two-text');
+// const secondHeroText = document.querySelector('.second-hero-text');
 
 const init = () => {
 
@@ -17,6 +19,8 @@ const init = () => {
     headerImg.style.transform = `translate3D(0, ${(-scroll*4)}px, 0)`;
     // logo.style.transform = `translate3D(${(scroll/5)}px, 0, 0)`;
     floatingText.style.cssText = `transform: translate3D(0, ${(-scroll/3)}px, 0); filter: blur(${(scroll/80)}px);`;
+    headerTwoText.style.cssText = `transform: translate3D(${(scroll/6)}px,0 , 0);`;
+    // secondHeroText.style.cssText = `transform: translate3D(${(scroll/6)}px,0 , 0);`;
   });
 
   function raf(time) {
